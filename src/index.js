0b24036f87389c6701cb3c8d9483c4aef8cf5f40
emily baier <emilyrbaier@gmail.com>
2019-05-13 13:12:45 -0700

    webpack play

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-console.log('hi');
+import styles from './index.css';
 
+const dogsArray = ['spot', 'rover', 'bingo', 'joe'];
+
+const root = document.getElementById('root');
+const ul = document.createElement('ul');
+ul.className = styles.dogs;
+root.appendChild(ul);
+
+dogsArray.forEach(dog => {
+  const liDog = document.createElement('li');
+  liDog.textContent = dog;
+  ul.appendChild(liDog);
+});
